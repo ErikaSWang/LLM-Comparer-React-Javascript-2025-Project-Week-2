fc8b45bf8b53aaf54a5a210f56088cb2ac446501
ErikaSWang <96995020+ErikaSWang@users.noreply.github.com>
2025-01-09 00:57:20 +0000

    Restored to 'f4d4664ce2d0f0be81bb6acdc006853837fc251d'
    Replit-Restored-To:f4d4664ce2d0f0be81bb6acdc006853837fc251d

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card} from 'react-bootstrap';
 import {Form} from 'react-bootstrap';
 
-export default function Results({outputPerplexity, outputOpenai, outputGemini}) {
+export default function Results({outputPerplexity, outputOpenai, outputAnthropic, outputGemini}) {
     return (
       <>
         <Form>
@@ -10,19 +10,25 @@ export default function Results({outputPerplexity, outputOpenai, outputGemini}) 
         </Form>
         <Card className="p-3 mx-3 mb-3">
           <Card.Body>
-            <Card.Title>Perplexity</Card.Title>
+            <Card.Title>Perplexity Llama</Card.Title>
             <Card.Text>{outputPerplexity}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="p-3 mx-3 mb-3">
           <Card.Body>
-            <Card.Title>OpenAi</Card.Title>
+            <Card.Title>OpenAi GPT-4o</Card.Title>
             <Card.Text>{outputOpenai}</Card.Text>
           </Card.Body>
         </Card>
         <Card className="p-3 mx-3 mb-3">
           <Card.Body>
-            <Card.Title>Gemini</Card.Title>
+            <Card.Title>Anthropic Claude</Card.Title>
+            <Card.Text>{outputAnthropic}</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="p-3 mx-3 mb-3">
+          <Card.Body>
+            <Card.Title>Google Gemini Flash</Card.Title>
             <Card.Text>{outputGemini}</Card.Text>
           </Card.Body>
         </Card>
